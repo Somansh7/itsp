9f7cf87a792f4797989c5ad6435879df9c43047a
Somansh7 <somansh0709@gmail.com>
2017-05-11 15:25:43 +0530

    Create DrawingView.java

---
+++ drawing app/DrawingView.java
@@ -0,0 +1,20 @@
+package com.example.somanshchordia.myapplication;
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by somansh chordia on 08/05/2017.
+ */
+
+public class DrawingView extends View {
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+    private void setupDrawing(){
+//get drawing area setup for interaction
+    }
+}
+
+
